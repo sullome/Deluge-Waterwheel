3b2caecee0058e335ca375fcac4475d54bf33135
Constantine Farrahov <sullome.techie@gmail.com>
2019-08-06 15:33:42 +0300

    Add copyright information

--- deluge_waterwheel/tests/test_ww_core.py
+++ deluge_waterwheel/tests/test_ww_core.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2019 Constantine Farrahov <sullome.techie@gmail.com>
+#
+# This file is part of Waterwheel and is licensed under GNU GPL 3.0, or later,
+# with the additional special exception to link portions of this program with
+# the OpenSSL library. See LICENSE for more details.
 
 from __future__ import print_function, unicode_literals
 
