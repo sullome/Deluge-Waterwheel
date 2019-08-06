0b0853e2017f692c5575f615b86da7078fa0208b
Constantine Farrahov <sullome.techie@gmail.com>
2019-08-06 17:08:15 +0300

    Drop Python 2 (mo-o-ore)

--- deluge_waterwheel/__init__.py
+++ deluge_waterwheel/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019 Constantine Farrahov <sullome.techie@gmail.com>
 #
 # Basic plugin template created by the Deluge Team.
@@ -6,6 +5,26 @@
 # This file is part of Waterwheel and is licensed under GNU GPL 3.0, or later,
 # with the additional special exception to link portions of this program with
 # the OpenSSL library. See LICENSE for more details.
+import sys
+if sys.version_info < (3,):
+    raise ImportError(
+    """You are running Waterwheel on Python 2
+
+Waterwheel are not compatible with Python 2, and you still 
+ended up with this version installed.
+That's unfortunate; sorry about that. It should not have happened.
+
+Make sure you have pip >= 9.0 to avoid this kind 
+of issue, as well as setuptools >= 24.2:
+
+ $ pip install pip setuptools --upgrade
+
+Your have to upgrade to Python 3.
+
+It would be great if you can figure out how this version ended up being
+installed, and try to check how to prevent that for future users.
+""")
+
 from deluge.plugins.init import PluginInitBase
 
 
